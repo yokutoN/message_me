e0a7aa424990dc20ac0032563fdd2e5a22e00edf
yokutoN <strex.5575@gmail.com>
2020-09-17 23:08:26 +0900

    メニューバー追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery
+//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
+
+$(document).ready(function(){ $('.ui.dropdown') .dropdown(); })
