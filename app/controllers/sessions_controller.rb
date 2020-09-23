0929593a5251e7c642c91d0a6a78b8bcde7613a2
yokutoN <strex.5575@gmail.com>
2020-09-23 20:55:31 +0900

    restrict controller layer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
-    def new
+    before_action :logged_in_redirect, only: [:new, :create]
 
+    def new
     end
 
     def create
@@ -20,4 +21,12 @@ class SessionsController < ApplicationController
         flash[:success] = "You have successfully logged out"
         redirect_to login_path
     end
+
+    private
+        def logged_in_redirect
+            if logged_in?
+                flash[:error] = "You are already logged in"
+                redirect_to root_path
+            end
+        end
 end
