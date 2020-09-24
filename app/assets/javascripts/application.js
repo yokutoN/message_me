ef11416d55846bbd323c8623f79f9df9b8e87d6c
yokutoN <strex.5575@gmail.com>
2020-09-24 16:37:02 +0900

    submit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,5 +37,6 @@ $(document).on('turbolinks:load', function(){
   $('.message .close').on('click', function(){
     $(this).closest('.message').transition('fade')
   });
+  submit_message();
   scroll_bottom();
 });
